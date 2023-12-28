8bebe526698fafba4127c727e41b9f2d46266d03
kipland-m <kiplandmelton@gmail.com>
2023-12-27 19:55:29 -0600

    recommit new approach to design

--- csskintool/src/App.js
+++ csskintool/src/App.js
@@ -1,13 +1,53 @@
+// App.js
 
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-function App() {
+import Home from './Home';
+import Weapons from './Weapons';
+import './App.css'; // Import the CSS file for styling
+
+// Function to import all images from the 'src/images' directory with the '.webp' extension
+const importAll = (r) => r.keys().map(r);
+
+// Create a context for images in the 'src/images' directory with the '.webp' extension
+const imageContext = require.context('./images', false, /\.(webp)$/);
+
+// Use the importAll function to create an array of imported image objects
+const imageArray = importAll(imageContext);
+
+
+const ImageDisplay = ({ images }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      {images.map((image, index) => (
+        <img
+          key={index}
+          src={image.default} // Using .default to access the imported image URL
+          alt={`Image ${index + 1}`}
+          style={{ width: '100px', height: '100px', margin: '5px' }}
+        />
+      ))}
     </div>
   );
-}
+};
+
+const App = () => {
+
+  return (
+    <Router>
+      <div className='App'>
+      <div>
+
+        <h1>CS2 SKIN TOOL</h1>
+        <ImageDisplay images={imageArray} />
+
+
+
+      </div>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
---
+++ csskintool/src/Weapons.js
@@ -0,0 +1,9 @@
+// components/About.js
+
+import React from 'react';
+
+const About = () => {
+  return <h2>Weapons</h2>;
+};
+
+export default About;
