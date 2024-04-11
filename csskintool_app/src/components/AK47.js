b34f396c01f24cbe7e790e76d48a9e56721559fd
kipland-m <kiplandmelton@gmail.com>
2024-04-10 19:37:17 -0500

    working skins displaying, now reuse same logic from this file on the rest of the weapons

--- csskintool_app/src/components/AK47.js
+++ csskintool_app/src/components/AK47.js
@@ -1,35 +1,40 @@
-// components/Home.js
 import React from 'react';
 
+// This function imports all images from the specified context
 const importAll = (r) => r.keys().map(r);
 
-// Create a context for images in the 'src/images' directory with the '.webp' extension
-const imageContext = require.context('../images/ak47', false, /\.(webp)$/);
-
-// Use the importAll function to create an array of imported image objects
+const imageContext = require.context('/src/images/ak47', false, /\.(png)$/);
 const imageArray = importAll(imageContext);
 
 
-/* This Function Handles the logic for the landing page, displaying all the weapons
-  and containing the logic to link to each weapon's skin repository*/
+const imageNames = imageArray.map(image => {
+  // Extract the filename from the full path
+  const fileName = image.split('/').pop();
+  const parts = fileName.split('.');
+  const weaponName = parts[0];
+  return weaponName;
+});
+
 const WeaponDisplay = ({ images }) => {
+  console.log(images); // Add this line for debugging
   return (
     <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-      {images.map((image, index) => (
-          <img
-            src={image}
-            alt={`Image ${index + 1}`}
-            style={{ width: '320px', height: '240px', margin: '10px', cursor: 'pointer' }}
-          />
+      {images && images.map((image, index) => (
+        <img
+          key={index}
+          src={image} // Assuming each image object has a 'default' property containing the image URL
+          alt={`Image ${index + 1}`}
+          style={{ width: '320px', height: '240px', margin: '10px', cursor: 'pointer' }}
+        />
       ))}
     </div>
   );
 };
 
-const AK47 = () => {
+const Weapons = () => {
   return (
-  <WeaponDisplay images={imageArray}/>
+  <WeaponDisplay images={imageArray} imageNames={imageNames}/>
   );
 };
 
-export default AK47;
+export default Weapons;
