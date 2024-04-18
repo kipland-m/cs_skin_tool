cbefe2d5be44823b91fc7d38b40038bf4f4fdd05
Michael-Ashby <msashby00@gmail.com>
2024-04-17 23:00:15 -0400

    Added display algorithm into all weapon component files.

--- csskintool_app/src/components/AUG.js
+++ csskintool_app/src/components/AUG.js
@@ -1,9 +1,40 @@
-// components/Home.js
-
 import React from 'react';
 
-const AUG = () => {
-  return <h2>aug</h2>;
+// This function imports all images from the specified context
+const importAll = (r) => r.keys().map(r);
+
+const imageContext = require.context('/src/images/aug', false, /\.(png)$/);
+const imageArray = importAll(imageContext);
+
+
+const imageNames = imageArray.map(image => {
+  // Extract the filename from the full path
+  const fileName = image.split('/').pop();
+  const parts = fileName.split('.');
+  const weaponName = parts[0];
+  return weaponName;
+});
+
+const WeaponDisplay = ({ images }) => {
+  console.log(images); // Add this line for debugging
+  return (
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      {images && images.map((image, index) => (
+        <img
+          key={index}
+          src={image} // Assuming each image object has a 'default' property containing the image URL
+          alt={`Image ${index + 1}`}
+          style={{ width: '320px', height: '240px', margin: '10px', cursor: 'pointer' }}
+        />
+      ))}
+    </div>
+  );
 };
 
-export default AUG;
+const Weapons = () => {
+  return (
+  <WeaponDisplay images={imageArray} imageNames={imageNames}/>
+  );
+};
+
+export default Weapons;
--- csskintool_app/src/components/M4A1.js
+++ csskintool_app/src/components/M4A1.js
@@ -1,9 +1,40 @@
-// components/Home.js
-
 import React from 'react';
 
-const M4A1 = () => {
-  return <h2>m4a1</h2>;
+// This function imports all images from the specified context
+const importAll = (r) => r.keys().map(r);
+
+const imageContext = require.context('/src/images/m4a1s', false, /\.(png)$/);
+const imageArray = importAll(imageContext);
+
+
+const imageNames = imageArray.map(image => {
+  // Extract the filename from the full path
+  const fileName = image.split('/').pop();
+  const parts = fileName.split('.');
+  const weaponName = parts[0];
+  return weaponName;
+});
+
+const WeaponDisplay = ({ images }) => {
+  console.log(images); // Add this line for debugging
+  return (
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      {images && images.map((image, index) => (
+        <img
+          key={index}
+          src={image} // Assuming each image object has a 'default' property containing the image URL
+          alt={`Image ${index + 1}`}
+          style={{ width: '320px', height: '240px', margin: '10px', cursor: 'pointer' }}
+        />
+      ))}
+    </div>
+  );
 };
 
-export default M4A1;
+const Weapons = () => {
+  return (
+  <WeaponDisplay images={imageArray} imageNames={imageNames}/>
+  );
+};
+
+export default Weapons;
